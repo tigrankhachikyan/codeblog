eecd75a935d7e0b1f3720cdf0d5d2eeff4b0fa97
Tigran <tigrankhachikyan@gmail.com>
2018-11-12 10:06:54 +0400

    styling

--- src/components/PostView/PostBody.js
+++ src/components/PostView/PostBody.js
@@ -3,25 +3,14 @@ import  './index.css';
 
 
 class PostBody extends Component {
-    constructor(props){
-        super(props);
-
-        // this.state = {
-        //         title: 'Title',
-        //         postbody: 'Some text',
-        //         date: new Date()
-        //     }
-    }
-
-    render() {
-        return (
-            
-        <div className = 'postView' >
-            <h1> {this.props.title} </h1>
-            <p> {this.props.body} </p>
-            {/* <span> {this.props.date.toDateString()} </span> */}
-        </div>)
-    }
+  render() {
+    return (
+    <div className = 'postView' >
+      <h1> {this.props.title} </h1>
+      <p> {this.props.body} </p>
+      {/* <span> {this.props.date.toDateString()} </span> */}
+    </div>)
+  }
 }
 
 
