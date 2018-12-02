47d63f5474be3b0044d6e9189e8358103298b05e
GoharPetrosyan <petrosyan.goharam@gmail.com>
2018-12-03 00:06:25 +0400

    chenge sitting window

--- src/components/Account/Settings/UserSettings.js
+++ src/components/Account/Settings/UserSettings.js
@@ -1,7 +1,9 @@
 import React, { PureComponent } from 'react';
 import { connect } from "react-redux";
 import './index.css';
-import SwitchLabel from './Switch';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
 
 import {
   loadUserSettings,
@@ -14,30 +16,59 @@ class UserSettings extends PureComponent {
     super(props);
     this.state = {
       seconds: null,
-      userName: 'User'
+      userName: 'User',
+      checkedA: true,
+      disabled: false
+  
     }
   }
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.checked,
+                    disabled: ! this.state.disabled
+     });
+
+  };
+
+  
 
   render() {
     return (
       <div className = 'sittingConteiner'>
-        {/* <div className = 'sittingDiv'> */}
             <h2>User Settings</h2>
-        {/* </div> */}
         <div className = 'hr'></div>
         
         <div className='switchStyle'>
-        <SwitchLabel/>
+        <FormGroup row>
+          <FormControlLabel
+            label="AutoSave"
+            control={
+              <Switch
+                checked={this.state.checkedA}
+                onChange={this.handleChange('checkedA')}
+                value="checkedA"
+                color="primary"
+              />
+            }
+          />        
+      </FormGroup>
         </div>
+        <p className='titleAutosave'> AutoSave Interval </p>
         <input 
             type="number" 
             placeholder='Seconds'
             className='SecondsInput'
             onChange={e => this.setState({seconds: e.target.value * 1000 })}
+            disabled={this.state.disabled}
       />
-      <button className='userButton'> 
-          {this.state.userName}
-      </button>
+        <p className='title'> Your username </p>
+            <input
+              className ='userName'
+              type='text'
+              value={this.state.userName}
+              onChange={e => this.setState({userName: e.target.value})}
+            />
+
 
       <button className='saveSetting'>
         Save
