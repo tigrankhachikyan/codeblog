8797c536ad2a7b465689b265d8d3f9913619e180
GoharPetrosyan <petrosyan.goharam@gmail.com>
2018-12-05 01:13:06 +0400

    new breanche Modal

--- src/components/Account/DashBoard/UserPostsTable.js
+++ src/components/Account/DashBoard/UserPostsTable.js
@@ -11,6 +11,7 @@ import Paper from '@material-ui/core/Paper';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
+import './index.css';
 
 
 const CustomTableCell = withStyles(theme => ({
